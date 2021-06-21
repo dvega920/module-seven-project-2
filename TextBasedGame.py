9340708b89f31fcd45fc112aedb29e85d559b67f
David Vega <dvega920@gmail.com>
2021-06-20 21:27:16 -0400

    Deleted check_inventory function and created instructions function, fixed format, removed unecessary comments

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -2,23 +2,32 @@
 # Southern New Hampshire University
 # IT-140 - Intro to Scripting
 
-def check_inventory(rooms, current_room, inventory):
-    """ function is passed in rooms, current_room, and inventory.
-    checks to see if the item in the current room is already in the inventory list.
-    if it is, a message is displayed to user that item already exists.
-    """
-    if (len(inventory) - 1) == 6:
-        print('CONGRATULATIONS BRODY!')
-        print('YOU SAVED COOPER!')
-    pass
+
+def instructions():
+    #  Below print statements print the heading of the game
+    print()
+    print('*' * 50)
+    print('            ADVENTURES OF BRODY:')
+    print('        DOG CATCHER COOPER SNATCHER')
+    print()
+    print('                INSTRUCTIONS')
+    print('Help Brody the yellow lab find his brother Cooper')
+    print('by collecting all 6 items and defeat the evil dog catcher')
+    print('before he sells Cooper.')
+    print()
+    print('  Move commands: North, South, East, West, exit')
+    print('*' * 50)
+    print()
+
 
 def check_for_items(rooms, current_room, inventory):
     """Function to check to see if items is either an empty string or 'Dog Catcher'
-If it neither then it checks to see if the item already exists in inventory.
-if not, prompts the user if they would like to add it to inventory.
-if user enters 'Yes', item is appended to inventory list and inventory list is shown. resets rooms item key to empty
-string so that when user enters that room again it will not prompt them to re-add the item instead lets them know that
-they already have that item if user enters 'No', then only the inventory list is displayed."""
+    If it is neither then it checks to see if the item already exists in inventory.
+    If not, prompts the user if they would like to add it to their inventory.
+    If user enters 'Yes', item is appended to inventory list and inventory list is shown. resets rooms item key to empty
+    string so that when user enters that room again it will not prompt them to re-add the item instead will display
+    room is empty.
+    If user enters 'No', then only the inventory list is displayed."""
     if rooms[current_room]['item'] != '' and rooms[current_room]['item'] != 'Dog Catcher':
         print('You see a {}'.format(rooms[current_room]['item']))
         add_item = input('Add to inventory? Yes or No? ')
@@ -39,16 +48,15 @@ they already have that item if user enters 'No', then only the inventory list is
     if rooms[current_room]['item'] == '':
         print('Room is empty.')
 
+
 def show_status(rooms, current_room, inventory):
     """ function takes in rooms, current_rooms, and inventory
-    prints the users current location.
-    function call check_for_items() to check if the room has any items
+    prints the users inventory count and current location.
+    function call check_for_items() to check if the room has any items in it.
     """
     print('Inventory Count: {}'.format(len(inventory)))
     print('You are in the {}.'.format(current_room))
     check_for_items(rooms, current_room, inventory)
-
-    # return current_room
 
 
 def user_prompt():
@@ -60,18 +68,20 @@ def user_prompt():
 
 
 def move_to_room(rooms, current_room, direction, inventory):
-    """function definition that takes in (3) params "rooms, "current_room," and "direction."
+    """function definition that takes in (4) params "rooms", "current_room", "direction", and "inventory"
     Funcs purpose is to move user between rooms.
     """
-    #  While loop runs the statements in its body while the current_room exists in the rooms dictionary.
+    #  While loop runs the statements in its body while the inventory is not equal to 6
+    #  and room item does not contain 'Dog Catcher'.
     while len(inventory) != 6 or rooms[current_room]['item'] != 'Dog Catcher':
-        check_inventory(rooms, current_room, inventory)
         show_status(rooms, current_room, inventory)
+        # Condition to check if inventory is equal to 6. User Wins game! loop breaks. Displays message.
         if len(inventory) == 6:
             print()
             print('CONGRATULATIONS BRODY!')
             print('YOU SAVED COOPER!')
             break
+        # Condition to check if room item equals "Dog Catcher" IF so, GAME OVER. loop breaks. Displays message
         if rooms[current_room]['item'] == 'Dog Catcher':
             print()
             print('YIKES!! DOG CATCHER!!!')
@@ -95,19 +105,9 @@ def move_to_room(rooms, current_room, direction, inventory):
         if next_move in rooms[current_room]:
             # show_status(rooms, current_room, inventory)  # Func call to show current_room.
             current_room = rooms[current_room][next_move]
-            # check_for_items(rooms, current_room, inventory)
         elif next_move not in direction:
             print("Invalid Entry - (Must enter: North, South, East, West, or exit)")
             print()
-        # elif rooms[current_room]['item'] == "Dog Catcher":
-        #     print('YIKES!! DOG CATCHER!!!')
-        #     print('Cooper\'s gone.')
-        #     print()
-        #     print('*' * 50)
-        #     print(' ' * 17, 'GAME OVER')
-        #     print('*' * 50)
-        #     print()
-        #     break
         else:
             print()
             print("You can\'t go that way.")
@@ -122,15 +122,7 @@ def main():
     static local variables are defined here.
     """
 
-    #  Below print statements print the heading of the game
-    print()
-    print('*' * 50)
-    print('            ADVENTURES OF BRODY:')
-    print('        DOG CATCHER COOPER SNATCHER')
-    print()
-    print('  Move commands: North, South, East, West, exit')
-    print('*' * 50)
-    print()
+    instructions()
 
     # A dictionary for the simplified dragon text game
     # The dictionary links a room to other rooms.
@@ -150,5 +142,6 @@ def main():
     current_room = 'Front Intake Room'
     inventory = []
     move_to_room(rooms, current_room, directions, inventory)
+
 
 main()  # function call to begin the game. Main entry point of game play.
